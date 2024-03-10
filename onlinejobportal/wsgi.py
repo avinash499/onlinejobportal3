17d64c294b45e05ff2444f73acddefb27e51f925
avinash499 <avinash499@gmail.com>
2024-03-10 10:44:39 +0530

    final commit

--- onlinejobportal/wsgi.py
+++ onlinejobportal/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinejobportal.settings')
 
 application = get_wsgi_application()
-
-app = application
