158e7427992954d489a306ca459107ab07cf867c
GKARTEEK <2200030564@kluniversity.in>
2024-03-16 10:46:20 +0530

    final commit

--- onlinejobportal/settings.py
+++ onlinejobportal/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
+SECRET_KEY = get_random_secret_key()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True  # Set to False in production
